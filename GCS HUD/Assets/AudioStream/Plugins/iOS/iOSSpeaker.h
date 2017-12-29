3a1fe60fec622622344644d201cf338b0ddf3acf
Miguel Sanchez <miguelsanchezangel@yahoo.com>
2017-12-29 11:04:23 -0800

    Removed Martin CvCvenGroš Unity Package files. Need to keep eye out on Copyrigt work

--- GCS HUD/Assets/AudioStream/Plugins/iOS/iOSSpeaker.h
+++
@@ -1,8 +0,0 @@
-// (c) 2016, 2017 Martin Cvengros. All rights reserved. Redistribution of source code without permission not allowed.
-// uses FMOD Studio by Firelight Technologies
-
-#import <Foundation/Foundation.h>
-
-void _RouteForPlayback();
-void _RouteForRecording();
-bool _externalDeviceConnected();
